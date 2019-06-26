1045aa8b231c0fd62496afa2f49f993e5c0f4b40
DESKTOP-ISK5I5H\Administrator <790609619@qq.com>
2019-06-26 18:09:46 +0800

    s

---
+++ src/components/HomeIndex.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="hello">
+	  <h1>{{ msg }}</h1>
+	</div>
+</template>
+
+<script>
+	export default
+	{
+		name: 'HomeIndex',
+		data () {
+		  return {
+		    msg: 'hello'
+		  }
+		}
+	}
+</script>
+
+<style>
+</style>
